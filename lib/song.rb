ec357e6c8a27680c2d97265e85c4f14a1147e5d1
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-04-06 21:02:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 class Song
   attr_accessor :name, :artist
-
+  attr_reader :song
   @all =[]
 
   def initialize(name)
@@ -20,4 +20,5 @@ class Song
   def artist_name
     @artist.name if @artist
   end
+  
 end
